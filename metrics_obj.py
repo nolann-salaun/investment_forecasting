34b2391a3874126557e5c90837b7fe195a26e4ed
NicolasDcz <nicolas.padc@outlook.com>
2025-05-25 23:27:15 +0200

    Added Sharpe ratio + volatility and portoflio Pnl + cumulative investment

--- metrics_obj.py
+++ metrics_obj.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import api_call_V2 as api
-from dateutil.relativedelta import relativedelta
 import numpy as np
 
 class Portfolio:
@@ -72,7 +71,7 @@ class Portfolio:
             df['cash'] = 0.0
             df['ETF_net_worth'] = 0.0
             df['ETF_PnL'] = 0.0
-            df['ETF_PnL %'] = 0.0
+            df['ETF_PnL_%'] = 0.0
             df['ticker'] = ticker  
             
             leftover = 0.0
@@ -93,13 +92,44 @@ class Portfolio:
                 df.at[idx, 'total_etf_units'] = total_units
                 df.at[idx, 'ETF_net_worth'] = total_units * price + leftover
                 df.at[idx, 'ETF_PnL'] = df.at[idx, 'ETF_net_worth'] - df.at[idx, 'etf_cumulative_investment']
-                df.at[idx, 'ETF_PnL %'] = ((df.at[idx, 'ETF_net_worth'] / df.at[idx, 'etf_cumulative_investment'] - 1) * 100).round(2)
+                df.at[idx, 'ETF_PnL_%'] = ((df.at[idx, 'ETF_net_worth'] / df.at[idx, 'etf_cumulative_investment'] - 1) * 100).round(2)
             
             dict_etf[ticker] = df
         
         # Merging ETF dataframe and sort per date
         consolidated_df = pd.concat(dict_etf).sort_index().sort_values(['Date', 'ticker'])
-        consolidated_df['cumulative_investment'] = consolidated_df['investment'].cumsum()
+
+        daily_totals_Inv = consolidated_df.groupby('Date')['investment'].sum().reset_index()
+        daily_totals_Inv['cumulative_investment'] = daily_totals_Inv['investment'].cumsum()
+        consolidated_df = pd.merge(
+            consolidated_df,
+            daily_totals_Inv[['Date', 'cumulative_investment']],
+            on='Date',
+            how='left'
+        )
+
+        daily_totals_PnL = consolidated_df.groupby('Date')['ETF_PnL'].sum().reset_index()
+        daily_totals_PnL['Portfolio_PnL'] = daily_totals_PnL['ETF_PnL']
+        consolidated_df = pd.merge(
+            consolidated_df,
+            daily_totals_PnL[['Date', 'Portfolio_PnL']],
+            on='Date',
+            how='left'
+        )
+        
+        daily_totals_networth = consolidated_df.groupby('Date')['ETF_net_worth'].sum().reset_index()
+        daily_totals_networth['Portfolio_net_worth'] = daily_totals_networth['ETF_net_worth']
+        consolidated_df = pd.merge(
+            consolidated_df,
+            daily_totals_networth[['Date', 'Portfolio_net_worth']],
+            on='Date',
+            how='left'
+        )
+
+        for idx in consolidated_df.index:
+            consolidated_df.at[idx, 'Portfolio_PnL_%'] = ((consolidated_df.at[idx, 'Portfolio_net_worth'] / consolidated_df.at[idx, 'cumulative_investment'] - 1) * 100).round(2)
+
+
         return consolidated_df
     
     def apply_CAGR_ratio(self):
@@ -116,17 +146,41 @@ class Portfolio:
         # Total CAGR
         total_net = cagr['ETF_net_worth'].sum()
         total_invest = cagr['etf_cumulative_investment'].sum()
+
         global_cagr = ((total_net / total_invest) ** (1 / self.investment_durations) - 1) * 100
+
         total_row = pd.DataFrame({
             'ETF_net_worth': [total_net],
             'etf_cumulative_investment': [total_invest],
-            'CAGR': [round(global_cagr, 4)]
+            'CAGR': [round(global_cagr, 2)]
         }, index=['TOTAL'])
         
         result = pd.concat([cagr, total_row])[['ETF_net_worth', 'etf_cumulative_investment', 'CAGR']]
         
         return result
     
+    def apply_SHARPE_ratio(self):
+        df = self.apply_ETF_purchase()
+        volatility_df = df.groupby('ticker')['ETF_PnL_%'].std().round(2)
+        volatility_df = volatility_df.to_frame(name='volatility')
+
+        global_volatility = df['Portfolio_PnL_%'].std().round(2)
+        total_row = pd.DataFrame({
+        'volatility': [global_volatility],
+        }, index=['TOTAL'])
+
+        volatility_df = pd.concat([volatility_df, total_row])[['volatility']]
+
+
+        '''Ratio de Sharpe = (Rendement du portefeuille - Rendement de l'actif sans risque) /
+        Volatilité du portefeuille'''
+
+        sharpe_ratio = ((df['Portfolio_PnL_%'].iloc[-1] - 2) / volatility_df['volatility'].iloc[-1]).round(2)
+
+        return volatility_df, sharpe_ratio
+        
+
+
 
 
 def main_metrics():
@@ -134,8 +188,11 @@ def main_metrics():
     df_result = portfolio.apply_monthly_investment()
     df_result = portfolio.apply_ETF_purchase()
     cagr = portfolio.apply_CAGR_ratio()
+    volatility, sharpe = portfolio.apply_SHARPE_ratio()
     print(df_result)
     print(cagr)
+    print(volatility)
+    print("Sharpe ratio :", sharpe)
 
 main_metrics()
 
